30f5a20b99f018614132d37484838865ad4fe5b9
RuRMZa <239465847+RramirezDev99@users.noreply.github.com>
2026-01-22 23:49:40 -0600

    fooooterrr222

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import logo from '../assets/logo.svg';
-import textlogo from '../assets/texticon.svg';
+import textlogo from '../assets/textIcon.svg';
 
 const Footer = () => {
   return (
