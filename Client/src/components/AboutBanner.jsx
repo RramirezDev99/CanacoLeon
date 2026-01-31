4e799394007088900d8b22f7e4a2911017dffa38
Ruben_RA <239465847+RramirezDev99@users.noreply.github.com>
2026-01-30 21:08:38 -0600

    nosotrosimagefix?

--- Client/src/components/AboutBanner.jsx
+++ Client/src/components/AboutBanner.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import "./AboutBanner.css";
 
-// src/components/AboutBanner.jsx
+// 1. IMPORTA LA IMAGEN AQUÍ
+// (Asegúrate que la ruta coincida con donde la pusiste)
+import bannerImg from "../../assets/blue-banner.jpg";
 
 const AboutBanner = () => {
   return (
     <section className="about-banner-container">
-      <div 
-        className="about-banner-image" 
-        style={{ 
-          // React buscará esto automáticamente en la carpeta public
-          backgroundImage: `url('public/blue-banner.jpg')`, 
+      <div
+        className="about-banner-image"
+        style={{
+          // 2. USA LA VARIABLE IMPORTADA
+          // Vite se encargará de poner la ruta correcta final
+          backgroundImage: `url(${bannerImg})`,
         }}
-      >
-        <div className="about-banner-overlay"></div>
-      </div>
+      ></div>
+
+      <div className="about-banner-overlay"></div>
 
       <div className="about-banner-content">
-          <h1>QUIENES SOMOS</h1>
+        <h1>QUIENES SOMOS</h1>
       </div>
     </section>
   );
